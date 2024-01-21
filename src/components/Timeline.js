a941786d1e3cd9f48966f14404e3ad4b5e37ac2c
hawraakhalil <hawraakhalil.05@gmail.com>
2024-01-21 19:18:34 +0200

    edit committees

--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -20,16 +20,19 @@ const Timeline = () => {
           color: "#fff",
         }}
       >
-        <h3>ACM ALUMNI DAY</h3>
+        <h3>
+          ACM Alumni Day: Bridging the Past and Present, Inspiring Futures 💬
+        </h3>
         <p>
-          The Alumni Day was a half-day fully in-person event where students got
-          to meet with Alumni and attend multiple short talks about internships,
-          interviews, work applications, graduate school, life abroad, and more
-          exciting topics! The talks featured 11 speakers who have worked or
-          studied at Amazon, EPFL, Google, IMC Trading, Meta, Microsoft,
-          University of Toronto, Walmart, and YouTube. Attendees enjoyed a lunch
-          break in between the talks and a networking session with the alumni at
-          the end of the day!
+          The Alumni Day was a half-day, fully in-person immersion of wisdom
+          event where students got to meet with accomplished alumni and attend
+          multiple short talks about internships, interviews, work applications,
+          graduate school, life abroad, and more exciting topics! The talks
+          featured 11 speakers, each with a unique journey, who have worked or
+          studied at powerhouse institutions such as Amazon, EPFL, Google, IMC
+          Trading, Meta, Microsoft, University of Toronto, Walmart, and YouTube.
+          Attendees enjoyed a relaxing lunch break in between the talks and a
+          networking session with the alumni at the end of the day.
         </p>
       </VerticalTimelineElement>
       <VerticalTimelineElement
@@ -43,15 +46,21 @@ const Timeline = () => {
           color: "#fff",
         }}
       >
-        <h3>Interview Workshop with Rene Adaimi, Founder of Resis</h3>
+        <h3>
+          Interview Workshop with Rene Adaimi: Decoding the Big-Tech Interview
+          Process 🎙️
+        </h3>
         <p>
-          The interview workshop was with Rene Adaimi, a software engineer
-          working at Google and the founder of Resis, a website that almost all
-          AUB students use to ease their registration processes! Rene explained
-          the interview process and provided important guidelines on how one
-          must approach their big-tech interview in a 1 hour workshop. Students
-          were excited to interact with Rene especially since he is somewhat of
-          a legend around campus.
+          The one-hour workshop was provided by Rene Adaimi, a software engineer
+          at Google and the visionary founder behind Resis – a website that has
+          become an indispensable tool for almost all AUB students streamlining
+          their registration processes. Rene provided a deep dive into the
+          intricacies of big-tech interviews, offering crucial guidelines on how
+          students should approach this critical phase of their career journey.
+          Excitement: The excitement among students was palpable as they engaged
+          with Rene, seizing the opportunity to interact with someone who has
+          not only conquered the tech industry but has also left an indelible
+          mark on campus life.
         </p>
       </VerticalTimelineElement>
       <VerticalTimelineElement
@@ -65,14 +74,16 @@ const Timeline = () => {
         contentStyle={{ background: "#47817F", color: "white" }}
         iconClassName="border"
       >
-        <h3>ACM AI Sessions</h3>
+        <h3>Launch of ACM AUB Artificial Intelligence Sessions 🤖</h3>
         <p>
-          The AI sessions happened once a week and featured alumni professionals
-          who work in the field of Artificial Intelligence (some notable
-          schools/workplaces include MIT and OpenAI). The alumni started the
-          first session by telling their career path story and providing insight
-          into their work. Then, they led a series of technical talks that
-          included tools to build AI models.
+          The AI sessions are held once a week and feature alumni professionals
+          who work in the field of Artificial Intelligence. Some of their
+          notable schools/workplaces include MIT and OpenAI. The sessions kick
+          off with a distinctive touch – alumni professionals share their career
+          path stories, offering personal insights into their journeys and work.
+          These talks then dive deep into the tools essential for building AI
+          models, offering a substantive understanding that goes beyond
+          superficial advertising to provide real knowledge about ML and AI.
         </p>
       </VerticalTimelineElement>
       <VerticalTimelineElement
@@ -86,14 +97,14 @@ const Timeline = () => {
         }}
         iconClassName="border"
       >
-        <h3>ACM CP Sessions</h3>
+        <h3>Launch of ACM AUB Competitive Programming Sessions 🏆</h3>
         <p>
-          The ACM CP sessions happened twice a week and featured experienced
-          students and alumni professionals who have reached ICPC and were able
-          to advance their careers well partially due to their CP efforts (some
-          notable workplaces include Google and Microsoft). The sessions given
-          provided technical training for students to participate in CP
-          competitions, by covering popular CP topics and solving exercises
+          The CP sessions are held twice a week and feature experienced students
+          and alumni professionals who reached ICPC and were able to navigate
+          their careers successfully, partially due to their CP efforts. Some of
+          their notable workplaces include Google and Microsoft. The sessions
+          given provided technical training for students to participate in CP
+          competitions by covering popular CP topics and solving exercises
           simultaneously.
         </p>
       </VerticalTimelineElement>
@@ -108,19 +119,22 @@ const Timeline = () => {
           color: "#fff",
         }}
       >
-        <h3>EECE 230X</h3>
+        <h3>EECE 230X: Empowering Minds Through Open Learning 🤝</h3>
         <p>
-          The EECE 230X initiative is a free, online course that is open to all
-          leaners and supported by the Faculty of Engineering through an
-          official certificate. All the material was made open source through a
-          website we created using a creative commons license. More than 2000
-          learners accessed the material. After 3 weeks of access to the
-          material, we administered an entrance exam for all students who wanted
-          to take it. The top-scoring students obtained access to a
+          The EECE 230X initiative is a free, online course, generously open to
+          all learners, serving as a beacon of knowledge supported by the
+          esteemed Faculty of Engineering through an official certificate upon
+          completion. Our commitment to accessibility and knowledge sharing is
+          epitomized by the release of all course materials under a creative
+          commons license and organized on a website our dedicated students
+          created. The initiative witnessed an overwhelming response, with more
+          than 2000 learners accessing the curated material. After 3 weeks of
+          launching, we administered an entrance exam for all students who
+          wanted to take it. The top-scoring students obtained access to a
           student-built auto-grading tool, teaching office hours, and the
           opportunity to earn a certificate upon passing the final exam. Almost
           50% of the students took the final exam and almost 50% of those who
-          took it passed, which is remarkable for an online course.
+          took it passed, which is a remarkable milestone.
         </p>
       </VerticalTimelineElement>
       <VerticalTimelineElement
@@ -134,12 +148,12 @@ const Timeline = () => {
           color: "#fff",
         }}
       >
-        <h3>FOUNDATION</h3>
+        <h3>FOUNDATION: ACM AUB's Inspirational Genesis 🌱</h3>
         <p>
-          Years later, our founders attended CP sessions given by the
-          predecessor students of Mr. Nahas and they were inspired. With the
-          support of the alumni of the CP community and Professor Louay Bazzi,
-          AUB ACM was born!
+          Years later, our founders discovered inspiration in the CP sessions
+          facilitated by the accomplished students of Mr. Nagi Nahas. Fueled by
+          this inspiration and fortified by the unwavering support of the CP
+          community alumni and Professor Louay Bazzi, ACM AUB was born.
         </p>
       </VerticalTimelineElement>
       <VerticalTimelineElement
@@ -153,17 +167,18 @@ const Timeline = () => {
           color: "#fff",
         }}
       >
-        <h3>PRE-FOUNDATION</h3>
+        <h3>PRE-FOUNDATION: Forging the Roots of Excellence🌟</h3>
         <p>
-          The inspiration for the official foundation of AUB ACM came from a
-          previously present competitive programming community within AUB. This
-          community has existed since 2007, when Mr. Nagi Nahas started giving
-          CP sessions to students to empower them to participate in programming
-          competitions. Our students shined in the LCPC, ACPC, and ICPC, often
-          ranking amongst the top 50-100 universities in the competition.
-          Throughout these years, our current advisor, Professor Louay Bazzi,
-          has always been a beacon of light to this community of students,
-          helping them build their careers and plan for their future.
+          Before the official establishment of AUB ACM, our journey began with
+          the spark of a competitive programming community at AUB. In 2007, the
+          visionary Mr. Nagi Nahas ignited this flame by conducting CP sessions,
+          empowering students to step into the thrilling realm of programming
+          competitions. Our students shined in the LCPC, ACPC, and ICPC, proudly
+          ranking amongst the top 50-100 universities in the competition, a
+          testament to the passion and dedication of our members. Behind every
+          success story is our esteemed advisor, Professor Louay Bazzi. A
+          guiding light for years, Professor Bazzi has been the cornerstone of
+          our community, shaping the careers and futures of countless students.
         </p>
       </VerticalTimelineElement>
     </VerticalTimeline>

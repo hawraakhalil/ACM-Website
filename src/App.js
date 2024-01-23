d2d9ad2e5a1f95194432a0562d4d492bd1c4a9dd
hawraakhalil <hawraakhalil.05@gmail.com>
2024-01-23 17:32:15 +0200

    HashRouter instead of BrowserRouter

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./App.css";
 import Navbar from "./components/Navbar";
-import { BrowserRouter as Router, Navigate,Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Navigate,Routes, Route } from "react-router-dom";
 import Home from "./components/pages/Home";
 import Board from "./components/pages/Board";
 import Events from "./components/pages/Events";
